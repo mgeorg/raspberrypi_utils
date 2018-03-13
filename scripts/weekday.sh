67f2656863a46a25a5dc79cdff3fbd943db877d3
Manfred Georg <manfred.georg@gmail.com>
2018-03-13 10:54:42 -0700

    Made weekday alarm ask for input.

--- scripts/weekday.sh
+++ scripts/weekday.sh
@@ -2,9 +2,5 @@
 
 set -eu
 
-bash /home/mgeorg/wakeup/scripts/alarm.sh
-bash /home/mgeorg/wakeup/scripts/warn.sh \
-    "Get ready to meditate in 7 minutes" "5" \
-    "2 minutes left" "2"
-bash /home/mgeorg/wakeup/scripts/meditation_session.sh 35 10
+python /home/mgeorg/wakeup/scripts/alarm.py
 
