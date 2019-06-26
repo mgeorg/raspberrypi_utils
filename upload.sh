6e34426071f340c74718da60f7b67b75345b4de9
Manfred Georg <mgeorg@google.com>
2019-06-26 14:40:05 -0700

    updating upload script more

--- upload.sh
+++ upload.sh
@@ -13,17 +13,26 @@ bash ~/wakeup/merge_master.sh
 
 git checkout master
 
+echo  "Merging master to feeder:"
 ssh feeder bash ~/wakeup/merge_master.sh
+echo  "pulling from feeder."
 git pull feeder master
 
+echo  "Merging master to pi:"
 ssh pi bash ~/wakeup/merge_master.sh
+echo  "pulling from pi."
 git pull pi master
 
+echo  "pushing to feeder"
 git push feeder master
+echo  "pushing to pi"
 git push pi master
 
+echo  "merging feeder"
 ssh feeder bash ~/wakeup/merge_branch.sh
+echo  "merging pi"
 ssh pi bash ~/wakeup/merge_branch.sh
+echo  "merging local pi"
 bash ~/wakeup/merge_branch.sh
 
 git checkout laptop
