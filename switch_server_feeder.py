ac5f725468791db56a8275e1cb55eb08b6ce9f24
Manfred Georg <manfred.georg@gmail.com>
2020-10-05 13:18:53 -0700

    Updated switch_server_feeder with new relay

--- switch_server_feeder.py
+++ switch_server_feeder.py
@@ -18,6 +18,12 @@ network_down_times = []
 last_network_reset_time = (
     datetime.datetime.now() - datetime.timedelta(minutes=60))
 
+# global last_feeder_start
+# global last_feeder_stop
+# last_feeder_start = None
+# last_feeder_stop = None
+
+skip_feeding_file = '/home/mgeorg/wakeup/data/skip_feeding.txt'
 
 def CheckPid(pid):
   """ Check For the existence of a unix pid. """
@@ -51,6 +57,17 @@ logging.basicConfig(filename='/home/mgeorg/feeder_log.txt',
                     level=logging.INFO)
 logging.info('Starting up.')
 
+def FileTouched(file_name, delete=False):
+  try:
+    with open(file_name, 'r') as f:
+      data = f.read().strip()
+    if delete:
+      os.remove(file_name)
+    return True
+  except IOError:
+    return False
+
+
 def GetNetworkLogger(name, log_file, level=logging.INFO):
   formatter = logging.Formatter('%(asctime)s %(message)s')
   handler = logging.FileHandler(log_file)
@@ -60,6 +77,22 @@ def GetNetworkLogger(name, log_file, level=logging.INFO):
   logger.addHandler(handler)
   return logger
 
+
+# Removed detection of feeder going off, since it didn't actually work anyway.
+# def FeederChange(channel):
+#   global last_feeder_start
+#   global last_feeder_stop
+#   now = datetime.datetime.now()
+#   if (last_feeder_start is None or
+#       last_feeder_start < (now - datetime.timedelta(seconds=30))):
+#     print('resetting from {} to {}'.format(last_feeder_start, now))
+#     last_feeder_start = now
+#   last_feeder_stop = now
+#   print('feeder start {}, stop {}, diff {}'.format(
+#             last_feeder_start, last_feeder_stop,
+#             last_feeder_stop-last_feeder_start))
+
+
 global network_log_file
 network_log_file = '/home/mgeorg/network_log.txt'
 global network_logger
@@ -68,16 +101,23 @@ network_logger.info('Starting up.')
 
 
 GPIO.setmode(GPIO.BCM)
-feeder_pin = 2  # Broadcom pin 02 (PI pin 3)
+feeder_pin = 27  # Broadcom pin 27 (PI pin 13)
 # Connect feeder pin to relay controller
 # (along with 3.3v (pin 1) and ground (pin 6)).
+# Make sure you're using a 3v relay, the 5v relays look like they
+# work but flake out randomly over time.
+GPIO.setup(feeder_pin, GPIO.OUT)
+
+# feeder_activated_pin = 17 # Broadcom pin 17 (PI pin 11)
+# GPIO.setup(feeder_activated_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.add_event_detect(feeder_activated_pin, GPIO.BOTH, callback=FeederChange)
+
 modem_pin = 3 # Broadcom pin 03 (PI pin 5)
 # Connect modem_pin and ground to modem relay switch.
-GPIO.setup(feeder_pin, GPIO.OUT)
 GPIO.setup(modem_pin, GPIO.OUT)
 
-# HIGH is off on the feeder relay.
-GPIO.output(feeder_pin, GPIO.HIGH)
+# LOW is off on the feeder relay.
+GPIO.output(feeder_pin, GPIO.LOW)
 # LOW is off on the modem relay.
 GPIO.output(modem_pin, GPIO.LOW)
 
@@ -108,20 +148,74 @@ Message creation time: {}
 """.format(subject, content, str(datetime.datetime.now())))
   logging.info('Finished sending/enqueuing, returncode = %d.' % p.returncode)
 
+def RoundTimeDelta(time_delta=None, round_to=datetime.timedelta(seconds=1)):
+   """Round a datetime.timedelta object to some number of seconds.
+   time_delta : datetime.timedelta object
+   rount_to : datetime.timedelta round to this interval
+   """
+   if time_delta is None : return None
+   microseconds = (time_delta.microseconds +
+                   (time_delta.seconds + time_delta.days * 24 * 3600) * 10**6)
+   round_to_microseconds = (round_to.microseconds +
+                   (round_to.seconds + round_to.days * 24 * 3600) * 10**6)
+   rounded_microseconds = (
+       (microseconds + round_to_microseconds/2) // round_to_microseconds
+       ) * round_to_microseconds
+   return datetime.timedelta(microseconds=rounded_microseconds)
 
 def Feed(num_sec, extra=None):
   logging.info('Feeding.')
   print('Feeding')
-  # Feeder relay turns on when control is grounded.
-  GPIO.output(feeder_pin, GPIO.LOW)
-  time.sleep(num_sec)
+  # Feeder relay turns on when control is powered.
   GPIO.output(feeder_pin, GPIO.HIGH)
+  time.sleep(num_sec)
+  GPIO.output(feeder_pin, GPIO.LOW)
+  time.sleep(1)
   if extra:
-    SendMail('Feeding Extra {}sec'.format(num_sec),
-             'Extra feeding succeeded ({}sec).'.format(num_sec))
+    subject_message = 'Feeding concluded (Extra)'
+    message = 'Extra feeding concluded'
   else:
-    SendMail('Feeding {}sec'.format(num_sec),
-             'Feeding succeeded ({}sec).'.format(num_sec))
+    subject_message = 'Feeding concluded'
+    message = 'Feeding concluded'
+  SendMail('{} {}sec'.format(subject_message, num_sec),
+           '{} {}sec'.format(message, num_sec))
+
+# def FeedWithTimeDetection(num_sec, extra=None):
+#   global last_feeder_start
+#   global last_feeder_stop
+#   last_feeder_start = None
+#   last_feeder_stop = None
+#   logging.info('Feeding.')
+#   print('Feeding')
+#   # Feeder relay turns on when control is grounded.
+#   GPIO.output(feeder_pin, GPIO.LOW)
+#   time.sleep(num_sec)
+#   GPIO.output(feeder_pin, GPIO.HIGH)
+#   time.sleep(1)
+#   succeeded = False
+#   time_fed = datetime.timedelta(seconds=0)
+#   if last_feeder_start is not None and last_feeder_stop is not None:
+#     time_fed = last_feeder_stop - last_feeder_start
+#     succeeded = True
+#   num_sec_fed = RoundTimeDelta(
+#       time_fed, datetime.timedelta(seconds=1)).total_seconds()
+#   print('measuring: feeder start {}, stop {}, time_fed {} ({}sec)'.format(
+#             last_feeder_start, last_feeder_stop,
+#             time_fed, num_sec_fed))
+#   if succeeded:
+#     if extra:
+#       subject_message = 'Feeding Extra'
+#       message = 'Extra feeding succeeded'
+#     else:
+#       subject_message = 'Feeding'
+#       message = 'Feeding succeeded'
+#     SendMail('{} {}sec ({})'.format(subject_message, num_sec, time_fed),
+#              '{} (meant for {}sec, got {}).'.format(
+#                  message, num_sec, time_fed))
+#   else:
+#     SendMail('FEEDING FAILED!!!',
+#              'FEEDING FAILED, meant for {} but got {}.'.format(
+#                  num_sec, time_fed))
 
 
 def NetworkIsDown():
@@ -208,7 +302,12 @@ def ControlLoop():
           extra = False
         if m.group(2):
           num_sec = int(m.group(2).strip())
-        Feed(num_sec, extra)
+        if FileTouched(skip_feeding_file, True):
+          print('skipped feeding {}sec'.format(num_sec))
+          SendMail('FEEDING SKIPPED!!!',
+                   'FEEDING SKIPPED, would have been {}sec.'.format(num_sec))
+        else:
+          Feed(num_sec, extra)
         continue
       m = re.match(r'cycle(?:\s+(\d+))?$', data)
       if m:
